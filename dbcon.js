668ef9202964e3488e0d4ba461d6432b603e65b0
Imad Farra <imad.farra60@gmail.com>
2021-01-30 00:22:52 +0200

    Add tags

--- dbcon.js
+++ dbcon.js
@@ -1,12 +1,12 @@
 var mysql = require('mysql');
 const env = require('dotenv').config()
-
+//connectin to database
 var con = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS
 });
-
+//feedback
 con.connect(function(err) {
   console.log("Connected!");
 });

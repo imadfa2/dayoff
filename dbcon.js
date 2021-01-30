d1a7c7e01f1ff6d2e3d088a4ac49d32a83d10db3
Imad Farra <imad.farra60@gmail.com>
2021-01-30 14:43:20 +0200

    4

--- dbcon.js
+++ dbcon.js
@@ -4,9 +4,25 @@ const env = require('dotenv').config()
 var con = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS
+  password: process.env.DB_PASS,
+  port : process.env.DB_PORT
+  
 });
 //feedback
 con.connect(function(err) {
-  console.log("Connected!");
+  if(err)
+  console.error(err);
+  else
+  console.error('connencted!');
 });
+
+// con.query('create table test',(error, results, fields)=>{
+//   console.log(results)
+//   console.log(fields)
+//   console.error(error)
+//   })
+  // con.query('select * from tab',(error, results, fields)=>{
+  // console.log(results)
+  // console.log(fields)
+  // })
+// export default con;

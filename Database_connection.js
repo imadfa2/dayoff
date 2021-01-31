0a07769d40f1a35138e591451117a43b247b0fc7
Imad Farra <imad.farra60@gmail.com>
2021-01-31 18:41:12 +0200

    m

--- Database_connection.js
+++ Database_connection.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 const pool  = mysql.createPool({
   connectionLimit : 10,
-  host            : process.env.DB_HOST,
-  user            : process.env.DB_USER,
-  password        : process.env.DB_PASS,
-  database        : process.env.DB_DB
+  host            : '213.239.205.250',
+  user            : 'root',
+  password        : 'mPdzF8%0nxtN',
+  database        : 'dayoff'
 })
 module.exports = pool; 

9ed4b4323f09eb82fa076516b3956ebc194760ff
Imadfarra <imad.farra60@gmail.com>
2021-02-18 18:38:41 +0200

    Done

--- Config/Token_Create.js
+++ Config/Token_Create.js
@@ -2,7 +2,5 @@ const jwt = require("jsonwebtoken");
 var username = null;
 
 module.exports = {
-  token: jwt.sign({ username }, "vX7xQr*45h&J", {
-    expiresIn: "48h",
-  }),
+  token: jwt.sign({ username }, "vX7xQr*45h&J", { expiresIn: "48h" }),
 };
